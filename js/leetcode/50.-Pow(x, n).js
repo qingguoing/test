c1421bd0e6bc31eb7219fbd2597d60b9c27a8e71
qingguoing <qingguoing@gmail.com>
2017-12-28 22:40:05 +0800

    chore: leetcode/50

---
+++ js/leetcode/50.-Pow(x, n).js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/powx-n/description/
+ * 
+Implement pow(x, n).
+
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+ */
+
+
+ /**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x, n);
+};
