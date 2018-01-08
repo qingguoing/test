4caa8da55900e335b5789c5dd60cdadad058a8f7
qingguoing <qingguoing@gmail.com>
2018-01-08 22:34:37 +0800

    chore: add new one

---
+++ js/leetcode/344-reverse-string.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/reverse-string/description/
+ * 
+ * Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+  return s.split('').reverse().join('');  
+};
