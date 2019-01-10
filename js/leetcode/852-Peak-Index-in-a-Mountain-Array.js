c4b4cf709fc8fd6010826f8eb95516f01fdc37fb
qingguoing <qingguoing@gmail.com>
2019-01-10 22:55:27 +0800

    feat: add leetcode 852

---
+++ js/leetcode/852-Peak-Index-in-a-Mountain-Array.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/
+ */
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(A) {
+  let i = 0;
+  const test = function test (arr) {
+      const len = arr.length;
+      if (len == 1) return i;
+      if (len == 2) return arr[1] > arr[0] ? ++i : i;
+      const center = Math.floor(len/2);
+      const left = arr[center - 1];
+      const right = arr[center];
+      if (left < right) {
+          i += center;
+          return test(arr.slice(center, len));
+      }
+      return test(arr.slice(0, center));
+  };
+  return test(A);
+};
