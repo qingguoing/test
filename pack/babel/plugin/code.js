448d7e53cdda2b3eaca999758ef0606c6ea8ce1e
qingguoing <qingguoing@gmail.com>
2019-03-23 15:46:50 +0800

    feat: add the comments

--- pack/babel/plugin/code.js
+++ pack/babel/plugin/code.js
@@ -1,14 +1,15 @@
-// @filter
-// const { a } = test;
-// const { a: b } = test;
-// const { a = 'xxx' } = test;
-// const { a: b = 'xxx' } = test;
+// @pipeline
+const { a1 } = test;
+// @pipeline-next-line-disabled
+const { a2: b2 } = test;
+const { a3 = 'xxx' } = test;
+const { a4: b4 = 'xxx' } = test; // @pipeline-line-disabled
 // const { a: { b: { c } } } = test;
 // const { a: { b = 'xxx' } } = test;
 // const { a: { b } = c } = test;
 // const { a: { b } = {} } = test;
 // let { a = 'b', b = 'c' } = _test;
 // const { a: { b } = c } = test;
-const test = {};
-const { c = 'xxx', ...b } = test;
-const { a = 'xxx' | 'aaa' | 'bbb' | 'ccc' | 'ddd' | 'eee', ...d } = test;
+// const test = {};
+// const { c = 'xxx', ...b } = test;
+// const { a = 'xxx' | 'aaa' | 'bbb' | 'ccc' | 'ddd' | 'eee', ...d } = test;
