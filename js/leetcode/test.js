a3104d985e0483a4603b74723c78c9c0bd9a2d4c
qingguoing <qingguoing@gmail.com>
2019-04-03 09:30:05 +0800

    feat: test

--- js/leetcode/test.js
+++ js/leetcode/test.js
@@ -8,14 +8,17 @@ var lexicalOrder = function(n) {
     const len = ('' + n).length;
     if (n < 10) return arr;
     for (let i = 1; i < 10; i++) {
+      for (let j = 0; j < len; j++) {
+        const indexZore = i * Math.pow(10, j);
+        if (indexZore <= n) {
+          res.push(indexZore);
+        }
+      }
       res = res.concat(arr.filter(num => {
         const numStr = '' + num;
-        for (let j = 0; j < len; j++) {
-          const indexZoreStr = ('' + i).padEnd(j, '0');
-          return numStr.indexOf(indexZoreStr) === 0;
-        }
         const index = '' + i;
-        return numStr.indexOf(index) === 0;
+        const second = numStr.charAt(1);
+        return (numStr.indexOf(index) === 0) && (!!second && second !== '0');
       }));
     }
     return res;
