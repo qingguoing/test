704bbc8f45b032744dcfce9d797e63ed2721f2b7
qingguoing <qingguoing@gmail.com>
2019-04-04 08:54:38 +0800

    feat: add 386

--- js/leetcode/386-Lexicographical-Numbers.js
+++ js/leetcode/386-Lexicographical-Numbers.js
@@ -25,3 +25,27 @@ var lexicalOrder = function(n) {
     });
     return arr;
 };
+
+
+/**
+ * 100ms, 43MB
+ * @param {*} n 
+ */
+var lexicalOrder2 = function(n) {
+  const res = [];
+  for (let i = 1; i < 10; ++i) {
+    dfs(i, n, res);
+  }
+  return res;
+
+  function dfs(cur, n, res) {
+    if (cur > n) return;
+    res.push(cur);
+    for (let i = 0; i < 10; ++i) {
+      if (cur * 10 + i > n) {
+        return;
+      }
+      dfs(cur * 10 + i, n, res);
+    }
+  }
+};
