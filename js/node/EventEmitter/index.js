76f1140f81acc78aae3be362a8c65bc856aed88c
qingguoing <qingguoing@gmail.com>
2019-05-25 15:44:29 +0800

    feat: add event-emitter test

--- js/node/EventEmitter/index.js
+++ js/node/EventEmitter/index.js
@@ -1,42 +1,69 @@
 class EventEmitter {
-  allListeners = {};
+  constructor() {
+    this.allListeners = {};
+  }
 
   on(name, fn) {
-    if (this.allListeners[name]) {
+    if (!this.allListeners[name]) {
       this.allListeners[name] = [fn];
     } else {
       this.allListeners[name].push(fn);
     }
+    return this;
   }
 
   off(name, fn) {
-    if (this.allListeners[name] > 1) {
-      
+    if(fn) {
+      if (!this.allListeners[name]) return;
+      const index = this.allListeners[name].indexOf(fn);
+      this.allListeners[name].splice(index, 1);
+    } else {
+      delete this.allListeners[name];
     }
-    delete this.allListeners[name];
-    return;
+    return this;
   }
 
   once(name, fn) {
-    this.on(name, function wrap() {
-      fn();
-      this.off(name, wrap);
+    const that = this;
+    let res, flag = false;
+    this.on(name, function wrap(...args) {
+      if (flag) {
+        return res;
+      }
+      flag = true;
+      res = fn(...args);
+      // that.off(name, wrap);
     });
+    return this;
   }
 
-  removeAllListeners(name, fn) {
-
-  }
-
-  removeListener(name, fn) {
-
-  }
-
-  listeners(name) {
-    return a.concat(this.allListeners[name] || []);
+  removeAllListeners() {
+    this.allListeners = {};
+    return this;
   }
 
   listenerCount(name) {
-    return (this.allListeners[name] || []).length;
+    const len = (this.allListeners[name] || []).length;
+    console.log(len);
+    return len;
+  }
+
+  emit(name, ...args) {
+    const listenerArr = this.allListeners[name] || [];
+    if (listenerArr.length > 0) {
+      listenerArr.forEach((listener) => {
+        listener.call(this, ...args);
+      });
+    }
+    return this;
   }
 }
+
+const test = new EventEmitter();
+function a(args) {
+  console.log('once|app', args);
+}
+test.once('once|app', a).off('once|app').emit('once|app', 'xxx').emit('once|app', 'yyy')
+.once('once2|app', function b(args) {
+  console.log('once2|app', args)
+}).emit('once2|app', 'xxx').emit('once2|app', 'yyy').removeAllListeners().listenerCount('once|app');
