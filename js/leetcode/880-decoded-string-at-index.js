980148de4219f210db2d99bd57c0b7a4bf94b3a9
qingguoing <qingguoing@gmail.com>
2019-05-29 22:39:58 +0800

    feat: add leetcode 880

---
+++ js/leetcode/880-decoded-string-at-index.js
@@ -0,0 +1,35 @@
+// 880: https://leetcode.com/problems/decoded-string-at-index/
+
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var decodeAtIndex = function(S, K) {
+  const len = S.length;
+  let size = 0;
+  for (let i = 0; i < len; ++i) {
+    const char = S.charAt(i);
+    const charNum = parseInt(char);
+    if (isNaN(charNum)) {
+      size++;
+    } else {
+      size *= charNum;
+    }
+  }
+
+  for (let i = len - 1; i >= 0; --i) {
+    K %= size;
+    const char = S.charAt(i);
+    const isChar = isNaN(parseInt(char));
+    if (K === 0 && isChar) {
+      return char;
+    }
+    if (!isChar) {
+      size = Math.round(size / parseInt(char));
+    } else {
+      size--;
+    }
+  }
+  return null;
+};
